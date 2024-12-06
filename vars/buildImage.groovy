95396ee403327bb7eb945dc0fd0cf690fb9dd606
Ausman Ginai <ausmanginai@googlemail.com>
2024-12-06 23:46:28 +0000

    Update buildImage.groovy

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -2,10 +2,8 @@
 
 def call() {
     echo "building the docker image"
-    withCredentials([usernamePassword(credentialsId: 'dockerhub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) { // credentials added in jenkins already
+    withCredentials([usernamePassword(credentialsId: 'dockerhub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) { 
         sh 'docker build -t ausman96/ausmandev:jma1.0 .'            // build image here.  ausman96 is account name, ausmandev is the docker hub repository name
         sh 'echo $PASS | docker login -u $USER --password-stdin'    // use password as an input (using echo, not actually providing live input)
         sh 'docker push ausman96/ausmandev:jma1.0'
-
-
 }
