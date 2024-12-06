653c3e53a8063d6ee1b3ca52d61f0ba02f389410
Ausman Ginai <ausmanginai@googlemail.com>
2024-12-06 23:47:43 +0000

    Update buildImage.groovy

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -6,4 +6,5 @@ def call() {
         sh 'docker build -t ausman96/ausmandev:jma1.0 .'            // build image here.  ausman96 is account name, ausmandev is the docker hub repository name
         sh 'echo $PASS | docker login -u $USER --password-stdin'    // use password as an input (using echo, not actually providing live input)
         sh 'docker push ausman96/ausmandev:jma1.0'
+    }   
 }
