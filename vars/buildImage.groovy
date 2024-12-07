963e83c77b887c80ebeb26bc9fb7dd0e96258eb4
Ausman Ginai <ausmanginai.ericsson@gmail.com>
2024-12-07 12:44:28 +0000

    parameter imagename added

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,10 +1,10 @@
 #!/user/bin/env groovy
 
-def call() {
+def call(String imagename) {
     echo "building the docker image"
     withCredentials([usernamePassword(credentialsId: 'dockerhub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) { 
-        sh 'docker build -t ausman96/ausmandev:jma1.0 .'            // build image here.  ausman96 is account name, ausmandev is the docker hub repository name
+        sh "docker build -t $imagename ."            // build image here.  ausman96 is account name, ausmandev is the docker hub repository name
         sh 'echo $PASS | docker login -u $USER --password-stdin'    // use password as an input (using echo, not actually providing live input)
-        sh 'docker push ausman96/ausmandev:jma1.0'
+        sh "docker push $imagename"
     }   
 }
